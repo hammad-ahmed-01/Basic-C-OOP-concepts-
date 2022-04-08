e194fe1dc80aec87bc662daf8da7f3362330bd2f
Hammad Ahmed <101525413+hammad-ahmed-01@users.noreply.github.com>
2022-04-08 20:33:21 +0500

    Add files via upload

---
+++ Basic OOP.cpp
@@ -0,0 +1,40 @@
+// Author == Hammad Ahmed
+// .cpp Microsoft VS
+
+// using iostream
+#include <iostream>
+
+// using std namespace
+using namespace std;
+
+// initializing class intro
+class intro 
+{
+// data members in a class are private by default so we have to label public 
+public:
+	string name;
+	int cms;
+	string sec;
+	float gpa;
+
+	void introduction()
+	{
+		cout << " My name is  " << name << "\n My CMS ID is  " << cms << "\n I am from section  " << sec << "\n My GPA is  " << gpa;
+	}
+	// constructor of the class
+	intro(string Name ,int CMS ,string Sec ,float GPA) 
+	{
+		name = Name;
+		cms = CMS;
+		sec = Sec;
+		gpa = GPA;
+	}
+};
+// main function
+int main() 
+{
+	// creating object my_intro in class intro
+	intro my_intro=intro("Hammad Ahmed",393404,"BSCS 11-A",3.5);
+	my_intro.introduction();
+	return 0;
+}
